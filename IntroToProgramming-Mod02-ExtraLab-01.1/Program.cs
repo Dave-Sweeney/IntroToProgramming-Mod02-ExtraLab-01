68da348ac66eff502e7a112cc1d78dfcf18c35f7
David J. Sweeney <david.j.sweeney@outlook.com>
2022-03-10 09:27:03 -0500

    Variable and constant declaration.

--- IntroToProgramming-Mod02-ExtraLab-01.1/Program.cs
+++ IntroToProgramming-Mod02-ExtraLab-01.1/Program.cs
@@ -1,17 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//class ComputeArea
+//{
+//    static void Main()
+//    {
+//        const double PI = 3.1415926535;
+//        radius = 12.5;
+//        area = PI * radius * radius;
+//        Console.WriteLine(" Circle area = {0} ", area);
+//    }
+//}
 
-namespace TestingNamespace
+/*
+ * The code above does not compile.  
+ * 
+ * What errors are reported, and at which lines?
+ *      * The name 'radius' does not exist in the current context - Line 7
+ *      * The name 'area' does not exist in the current context - Line 8
+ *      
+ * What do you think is the cause?
+ *      * The variables radius and area are not declared before they are used.
+ *      
+ * Fix the code so that it compiles and runs.
+ *      * See code below
+ *      
+ * How did you fix the errors?
+ *      * Declared radius and area as data type double
+ */
+
+using System;
+class ComputeArea
 {
-    internal class SayHello
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello");
-            Console.ReadLine();
-        }
+        const double PI = 3.1415926535;
+        double radius = 12.5;
+        double area = PI * radius * radius;
+        Console.WriteLine(" Circle area = {0} ", area);
     }
 }
