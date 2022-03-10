c6a593e1132f5e86e3dc0b3b507208214208166a
David J. Sweeney <david.j.sweeney@outlook.com>
2022-03-10 09:31:50 -0500

    Initial commit.

--- IntroToProgramming-Mod02-ExtraLab-01.1/Program.cs
+++ IntroToProgramming-Mod02-ExtraLab-01.1/Program.cs
@@ -1,40 +1,36 @@
 ﻿//using System;
-//class ComputeArea
+//class mod2lab2
 //{
 //    static void Main()
 //    {
-//        const double PI = 3.1415926535;
-//        radius = 12.5;
-//        area = PI * radius * radius;
-//        Console.WriteLine(" Circle area = {0} ", area);
+//        int f = 1.5 F;
+//        char s = " Harry Potter ";
+//        char c = ’A ’;
+//        Console.WriteLine(" f value = {0} , s = {1} , c = {2} ",
+//        f, s, c);
 //    }
 //}
 
 /*
- * The code above does not compile.  
- * 
- * What errors are reported, and at which lines?
- *      * The name 'radius' does not exist in the current context - Line 7
- *      * The name 'area' does not exist in the current context - Line 8
- *      
- * What do you think is the cause?
- *      * The variables radius and area are not declared before they are used.
- *      
- * Fix the code so that it compiles and runs.
- *      * See code below
- *      
- * How did you fix the errors?
- *      * Declared radius and area as data type double
+ *  Fix the above code so that it compiles and runs.  
+ *  How did you fix the errors?
+ *      * change the data type of variable 'f' to float.
+ *      * change the data type of variable 's' to string.
  */
 
 using System;
-class ComputeArea
+class mod2lab2
 {
     static void Main()
     {
-        const double PI = 3.1415926535;
-        double radius = 12.5;
-        double area = PI * radius * radius;
-        Console.WriteLine(" Circle area = {0} ", area);
+        float f = 1.5F;
+        string s = " Harry Potter ";
+        char c = 'A';
+        Console.WriteLine(" f value = {0} , s = {1} , c = {2} ",
+        f, s, c);
+
     }
 }
+
+
+
